1a561c9ab6b0e46ed64a6b93acfbe7440c41527d
Charon <charon.s.tom@gmail.com>
2023-10-20 10:54:52 +0200

    ajdusting header marging

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import SwitchLanguage from "./SwitchLanguage";
 function Header() {
   return (
     <header className="backdrop-blur-sm dark:bg-light-header w-full fixed z-20 shadow">
-      <div className="mx-14 flex justify-between items-center py-4 lg:px-10">
+      <div className="mx-5 md:mx-10 lg:mx-14 flex justify-between items-center py-4 lg:px-10">
         <div className="p-[8px] dark:text-secondary">
           <span className="font-semibold text-[18px]">Tom Charon</span>
         </div>
